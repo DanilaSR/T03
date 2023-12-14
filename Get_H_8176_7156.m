6d9e8053286a86ea206928d5bde229eacbfcc3ec
dillyric <dillyric@student.21-school.ru>
2023-12-14 18:06:42 +0300

    H is fixed

--- Get_H_8176_7156.m
+++ Get_H_8176_7156.m
@@ -1,17 +1,19 @@
 function H = Get_H_8176_7156()
 
 ones = load("H.txt");
+
 A = cell(2,16);
 
 for j = 1:2
     for i = 1:16
         firstRow = zeros(1, 511);
-        one = ones(j*i, :) + 1;
-        firstRow(one) = 1;
+        firstRow(1, ones(i + (j-1)*16, 1) + 1) = 1;
+        firstRow(1, ones(i + (j-1)*16, 2) + 1) = 1;
         A{j, i} = circulant(firstRow);
     end
 end
 
 H = cell2mat(A);
+
 end
 
